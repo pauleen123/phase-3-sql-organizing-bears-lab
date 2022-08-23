c0fcd1ffd02fc6dd45b9ad9da8eea9abc999d1f7
+ <+>
2022-08-23 15:11:04 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("KungFu Panda",10,"F","Black","High",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Moana Panda",10,"F","Black","High",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Chowder Panda",11,"M","White","Low",FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Kank Panda",11,"M","White","Low",FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mickey Panda",12,"F","Black","High",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Blackie Panda",12,"F","Black","High",TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Kartel Panda",13,"F","White","Low",FALSE);
+INSERT INTO bears (name,age, sex, color, temperament, alive) VALUES(NULL,13,"F","White","Low",FALSE);
+
